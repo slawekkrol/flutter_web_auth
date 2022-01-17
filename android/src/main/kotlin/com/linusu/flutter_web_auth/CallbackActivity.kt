d0554e86fecb9a2b0418657c02456ebb90037161
Slawomir Krol <skkrolu@gmail.com>
2022-01-17 13:56:06 +0100

    Implement android authentication as single page

--- android/src/main/kotlin/com/linusu/flutter_web_auth/CallbackActivity.kt
+++ android/src/main/kotlin/com/linusu/flutter_web_auth/CallbackActivity.kt
@@ -1,7 +1,7 @@
 package com.linusu.flutter_web_auth
 
 import android.app.Activity
-import android.net.Uri
+import android.content.Intent
 import android.os.Bundle
 
 class CallbackActivity: Activity() {
@@ -16,5 +16,10 @@ class CallbackActivity: Activity() {
     }
 
     finish()
+
+    val activity = FlutterWebAuthPlugin.activity!!
+    val newIntent = Intent(activity, activity.javaClass)
+    newIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+    activity.startActivity(newIntent)
   }
 }
--- android/src/main/kotlin/com/linusu/flutter_web_auth/FlutterWebAuthPlugin.kt
+++ android/src/main/kotlin/com/linusu/flutter_web_auth/FlutterWebAuthPlugin.kt
@@ -1,12 +1,13 @@
 package com.linusu.flutter_web_auth
 
-import android.content.Context
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
-
 import androidx.browser.customtabs.CustomTabsIntent
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -14,61 +15,81 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class FlutterWebAuthPlugin(private var context: Context? = null, private var channel: MethodChannel? = null): MethodCallHandler, FlutterPlugin {
-  companion object {
-    val callbacks = mutableMapOf<String, Result>()
+class FlutterWebAuthPlugin(private var channel: MethodChannel? = null) : MethodCallHandler, FlutterPlugin, ActivityAware {
+    companion object {
+        val callbacks = mutableMapOf<String, Result>()
 
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-        val plugin = FlutterWebAuthPlugin()
-        plugin.initInstance(registrar.messenger(), registrar.context())
+        @SuppressLint("StaticFieldLeak")
+        var activity: Activity? = null
+
+        @JvmStatic
+        fun registerWith(registrar: Registrar) {
+            val plugin = FlutterWebAuthPlugin()
+            plugin.initInstance(registrar.messenger())
+        }
+
     }
 
-  }
-
-  fun initInstance(messenger: BinaryMessenger, context: Context) {
-      this.context = context
-      channel = MethodChannel(messenger, "flutter_web_auth")
-      channel?.setMethodCallHandler(this)
-  }
-
-  override public fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-      initInstance(binding.getBinaryMessenger(), binding.getApplicationContext())
-  }
-
-  override public fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-      context = null
-      channel = null
-  }
-
-  override fun onMethodCall(call: MethodCall, resultCallback: Result) {
-    when (call.method) {
-        "authenticate" -> {
-          val url = Uri.parse(call.argument("url"))
-          val callbackUrlScheme = call.argument<String>("callbackUrlScheme")!!
-          val preferEphemeral = call.argument<Boolean>("preferEphemeral")!!
-
-          callbacks[callbackUrlScheme] = resultCallback
-
-          val intent = CustomTabsIntent.Builder().build()
-          val keepAliveIntent = Intent(context, KeepAliveService::class.java)
-
-          intent.intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
-          if (preferEphemeral) {
-              intent.intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
-          }
-          intent.intent.putExtra("android.support.customtabs.extra.KEEP_ALIVE", keepAliveIntent)
-
-          intent.launchUrl(context!!, url)
-        }
-        "cleanUpDanglingCalls" -> {
-          callbacks.forEach{ (_, danglingResultCallback) ->
-              danglingResultCallback.error("CANCELED", "User canceled login", null)
-          }
-          callbacks.clear()
-          resultCallback.success(null)
-        }
-        else -> resultCallback.notImplemented()
+    fun initInstance(messenger: BinaryMessenger) {
+        channel = MethodChannel(messenger, "flutter_web_auth")
+        channel?.setMethodCallHandler(this)
     }
-  }
+
+    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        initInstance(binding.binaryMessenger)
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        activity = null
+        channel = null
+    }
+
+    override fun onMethodCall(call: MethodCall, resultCallback: Result) {
+        when (call.method) {
+            "authenticate" -> {
+                val url = Uri.parse(call.argument("url"))
+                val callbackUrlScheme = call.argument<String>("callbackUrlScheme")!!
+                val preferEphemeral = call.argument<Boolean>("preferEphemeral")!!
+
+                callbacks[callbackUrlScheme] = resultCallback
+
+                val intent = CustomTabsIntent
+                        .Builder()
+                        .setUrlBarHidingEnabled(true)
+                        .setShareState(CustomTabsIntent.SHARE_STATE_OFF)
+                        .setShowTitle(false)
+                        .build()
+                val keepAliveIntent = Intent(activity, KeepAliveService::class.java)
+                if (preferEphemeral) {
+                    intent.intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+                }
+                intent.intent.putExtra("android.support.customtabs.extra.KEEP_ALIVE", keepAliveIntent)
+                intent.launchUrl(activity!!, url)
+            }
+            "cleanUpDanglingCalls" -> {
+                callbacks.forEach { (_, danglingResultCallback) ->
+                    danglingResultCallback.error("CANCELED", "User canceled login", null)
+                }
+                callbacks.clear()
+                resultCallback.success(null)
+            }
+            else -> resultCallback.notImplemented()
+        }
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        activity = null
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivity() {
+        activity = null
+    }
 }
